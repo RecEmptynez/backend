841e8f4acc1cb116ef9715420bafaae70168fe29
EricErlandssonHollgren <eric.e.hollgren@gmail.com>
2023-03-29 11:03:53 +0200

    comments

--- backend/core/config.py
+++ backend/core/config.py
@@ -16,6 +16,5 @@ class Settings:
     POSTGRES_PORT : str = os.getenv("POSTGRES_PORT",5432) # default postgres port is 5432
     POSTGRES_DB : str = os.getenv("POSTGRES_DB","tdd")
     DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
-    #DATABASE_URL = os.getenv("CONNECTION_STRING")
 
 settings = Settings()
